2be96d198e151f05de8347424d8bda71bf1926a6
shuja <shuja.gmail.com>
2023-06-14 11:40:31 +0530

    length function

--- roboshop-setup/ex1/main.tf
+++ roboshop-setup/ex1/main.tf
@@ -1,9 +1,12 @@
 resource "aws_instance" "ec2" {
-  count = 5
+  count = length(var.instances)
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
   }
+}
+variable "instances" {
+  default = ["cart","catalogue","shipping","payment","user"]
 }
