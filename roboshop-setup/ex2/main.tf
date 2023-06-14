22cdf8c7b3874a39c22ea73199f5d36abfb02daf
shuja <shuja.gmail.com>
2023-06-14 11:56:03 +0530

    example list output

---
+++ roboshop-setup/ex2/main.tf
@@ -0,0 +1,15 @@
+variable "d1" {
+  default = [
+    {
+      course_name = "aws"
+      trainer_name = "shuja"
+    },
+    {
+      course_name = "devops"
+      trainer_name = "Mohd"
+    }
+  ]
+}
+output "d1-val" {
+  value = var.d1.*.course_name
+}
