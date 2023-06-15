42c863824138f17f6d6923bd205e6182bfcf06f3
shuja <shuja.gmail.com>
2023-06-15 11:36:38 +0530

    sg id referred in ec2 by module

--- roboshop-setup/ex6/main.tf
+++ roboshop-setup/ex6/main.tf
@@ -3,12 +3,12 @@ module "ec2" {
   source = "./ec2"
   component = each.value["name"]
   instance_type = each.value["type"]
+  sg_id  = module.sg.sg_id
 }
 
 module "sg" {
   source = "./sg"
+
 }
 
-output "sg_id" {
-  value = module.sg
-}
+
