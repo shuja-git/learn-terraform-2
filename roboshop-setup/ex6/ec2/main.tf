dbebfb39914ec38de69f1d6c4beeeab8d4695252
shuja <shuja.gmail.com>
2023-06-15 11:23:04 +0530

    sg

--- roboshop-setup/ex6/ec2/main.tf
+++ roboshop-setup/ex6/ec2/main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ami" {
   resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-
+  vpc_security_group_ids = []
   tags = {
     Name = var.component
   }
---
+++ roboshop-setup/ex6/sg/main.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "sg" {
+name = "sg-allow-all"
+description = "Allow HTTP and SSH traffic via Terraform"
+
+ingress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+}
+output "sg_id" {
+  value = aws_security_group.sg.id
+}
