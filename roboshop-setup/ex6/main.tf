6276ae194574411a9969d7cc058485d25bd6c754
shuja <shuja.gmail.com>
2023-06-15 12:21:50 +0530

    getting component name

--- roboshop-setup/ex6/main.tf
+++ roboshop-setup/ex6/main.tf
@@ -14,7 +14,7 @@ module "route53" {
   for_each = var.instances
   source = "./route53"
   component = each.value["name"]
-  private_ip = module.ec22["cart"].private_ip
+  private_ip = module.ec22[each.value["name"]].private_ip
 }
 
 output "private-ip" {
