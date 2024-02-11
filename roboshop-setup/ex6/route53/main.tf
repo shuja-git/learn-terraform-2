6b6c0c8d7315d301fac24281937a1b8cc019d709
shuja-git <shuja.devops@gmail.com>
2024-02-11 15:05:46 +0530

    ec2 and dns in module

--- roboshop-setup/ex6/route53/main.tf
+++ roboshop-setup/ex6/route53/main.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "record" {
-  zone_id = "Z10218511FGAD8YC6L1HI"
+  zone_id = "Z10377495CKDE7OXJB1E"
   name    = "${var.component}-dev.shujadevops.online"
   type    = "A"
   ttl     = 300
